330d8aeb7daae9ef2fa8ad5b62fed256f587b92b
Eric Mylenbusch <e.mylenbusch@gmx.de>
2017-02-12 15:31:47 +0100

    Szene: Carsten, neues Skript: TimeInteraction. GUI auf Buttons gelegt, Farbe ändert sich beim Drücken. Auswirkungen auf die Szene haben sie noch nicht.

---
+++ 01_unity/Assets/Scripts/TimeInteraction.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeInteraction : MonoBehaviour {
+
+	SteamVR_TrackedObject trackedObj;
+	public RawImage playButton;
+	public RawImage pauseButton;
+	public RawImage backButton;
+	public RawImage forwardButton;
+	public Color normal;
+	public Color selected;
+	private SteamVR_Controller.Device device;
+	private bool isPaused = true;
+	private bool menuPressed = false;
+
+	// Use this for initialization
+	void Start () {
+		playButton.color = normal;
+		pauseButton.color = normal;
+		forwardButton.color = normal;
+		backButton.color = normal;
+		pauseButton.enabled = false;
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+
+	void FixedUpdate () {
+		device = SteamVR_Controller.Input((int)trackedObj.index);
+
+		if (device.GetPressUp (SteamVR_Controller.ButtonMask.ApplicationMenu)) {
+
+			if (isPaused) {
+				playButton.color = normal;
+				playButton.enabled = false;
+				pauseButton.enabled = true;
+			
+			} else {
+				pauseButton.color = normal;
+				pauseButton.enabled = false;
+				playButton.enabled = true;
+			}
+
+			isPaused = !isPaused;
+		}
+
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.ApplicationMenu)) {
+			if (isPaused) playButton.color = selected;
+			else pauseButton.color = selected;
+		}
+
+
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad)) {
+
+			Vector2 touchpad = (device.GetAxis (Valve.VR.EVRButtonId.k_EButton_Axis0));
+
+			// Forward Button gedrückt
+			if (touchpad.x > 0.15f) {
+				forwardButton.color = selected;
+			}
+
+			// Backward Button gedrückt
+			if (touchpad.x < -0.15f) {
+				backButton.color = selected;
+			}
+		}
+
+		if (device.GetPressUp (SteamVR_Controller.ButtonMask.Touchpad)) {
+			forwardButton.color = normal;
+			backButton.color = normal;
+		}
+	}
+
+
+}
